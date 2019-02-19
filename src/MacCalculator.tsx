e4c5a40eca5245c944c669b54858644c18dbaa4c
Daniel Zelaya <wupaz9@gmail.com>
2019-02-18 23:39:15 -0600

    Add calculator controls

--- src/MacCalculator.tsx
+++ src/MacCalculator.tsx
@@ -2,14 +2,26 @@ import React, { useState, useEffect } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import Display from './components/Display';
 import theme from './theme';
+import ControlGrid from './components/ControlGrid';
+import Control from './components/Control';
 
 const Wrapper = styled.div`
   background-color: #444;
   border-radius: 4px;
 `;
 
+const LeftBorderControl = styled(Control)`
+  border-bottom-left-radius: 4px;
+`;
+
+const RightBorderControl = styled(Control)`
+  border-bottom-right-radius: 4px;
+`;
+
 const MacCalculator = () => {
   const [displayValue, setDisplayValue] = useState('0');
+  const initialControlsBgColor = '#555';
+  const operatorColumnBgColor = 'orange';
 
   useEffect(() => {
     const handleKeypress = ({ which, key }: KeyboardEvent) => {
@@ -46,6 +58,72 @@ const MacCalculator = () => {
     <ThemeProvider theme={theme}>
       <Wrapper>
         <Display value={displayValue} />
+        <ControlGrid>
+          <Control bgColor={initialControlsBgColor} column={1} row={1}>
+            AC
+          </Control>
+          <Control bgColor={initialControlsBgColor} column={2} row={1}>
+            +/-
+          </Control>
+          <Control bgColor={initialControlsBgColor} column={3} row={1}>
+            %
+          </Control>
+          <Control bgColor={operatorColumnBgColor} column={4} row={1}>
+            ÷
+          </Control>
+          <Control column={1} row={2}>
+            7
+          </Control>
+          <Control column={2} row={2}>
+            8
+          </Control>
+          <Control column={3} row={2}>
+            9
+          </Control>
+          <Control bgColor={operatorColumnBgColor} column={4} row={2}>
+            x
+          </Control>
+          <Control column={1} row={3}>
+            4
+          </Control>
+          <Control column={2} row={3}>
+            5
+          </Control>
+          <Control column={3} row={3}>
+            6
+          </Control>
+          <Control bgColor={operatorColumnBgColor} column={4} row={3}>
+            -
+          </Control>
+          <Control column={1} row={4}>
+            1
+          </Control>
+          <Control column={2} row={4}>
+            2
+          </Control>
+          <Control column={3} row={4}>
+            3
+          </Control>
+          <Control bgColor={operatorColumnBgColor} column={4} row={4}>
+            +
+          </Control>
+          <Control bgColor={initialControlsBgColor} column={1} row={1}>
+            AC
+          </Control>
+          <LeftBorderControl column={1} row={5} columnCount={2}>
+            0
+          </LeftBorderControl>
+          <Control column={3} row={5}>
+            .
+          </Control>
+          <RightBorderControl
+            bgColor={operatorColumnBgColor}
+            column={4}
+            row={5}
+          >
+            =
+          </RightBorderControl>
+        </ControlGrid>
       </Wrapper>
     </ThemeProvider>
   );
