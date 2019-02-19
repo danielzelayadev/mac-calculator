048dcb36479c03b4cee848f60ffa17492f7b49f1
Daniel Zelaya <wupaz9@gmail.com>
2019-02-18 23:37:30 -0600

    Setup ControlGrid

---
+++ src/components/ControlGrid/index.tsx
@@ -0,0 +1,14 @@
+import styled, { ThemedStyledProps } from 'styled-components';
+import { Theme } from '../../theme';
+
+type Props = ThemedStyledProps<{}, Theme>;
+
+const ControlGrid = styled.div`
+  display: grid;
+  font-family: ${({ theme }: Props) => theme.fontFamily};
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-gap: 1px;
+`;
+
+export default ControlGrid;
