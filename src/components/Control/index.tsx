75b9b77405ec1ea5086aa2e3a3072930a1b58cb1
Daniel Zelaya <wupaz9@gmail.com>
2019-02-18 23:37:45 -0600

    Add Control component

---
+++ src/components/Control/index.tsx
@@ -0,0 +1,29 @@
+import styled, { StyledProps } from 'styled-components';
+
+type Props = StyledProps<{
+  column: number;
+  row: number;
+  bgColor?: string;
+  columnCount?: number;
+  rowCount?: number;
+  textColor?: string;
+  tooltip?: string;
+  onClick?: Function;
+}>;
+
+const Control = styled.div`
+  ${(props: Props) => `
+    background-color: ${props.bgColor || '#777'};
+    color: ${props.textColor || '#fff'};
+    grid-column: ${props.column};
+    grid-row: ${props.row};
+    grid-column-end: ${props.column + (props.columnCount || 1)};
+    grid-row-end: ${props.row + (props.rowCount || 1)};
+  `}
+  font-size: 24px;
+  line-height: 71px;
+  height: 71px;
+  text-align: center;
+`;
+
+export default Control;
