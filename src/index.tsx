9b0d8d1fd09d87aab524c4ceae568e194b074af8
Daniel Zelaya <wupaz9@gmail.com>
2019-02-19 22:04:19 -0600

    Add page styles and colors

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MacCalculator from './MacCalculator';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<MacCalculator />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <h1 className="title">React Mac Calculator</h1>
+    <MacCalculator />
+    <div className="credits-ctr">
+      <p className="credits">Made with 💙by Daniel Zelaya.</p>
+    </div>
+  </div>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
